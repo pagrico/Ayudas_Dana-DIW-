a9b2a56c28cd7a8cb80785c5e9ab74421fe548b8
Pablo Grimao Company <Pablogrimaocompany@gmail.com>
2024-12-01 00:22:11 +0100

    Primer commit

---
+++ js/Ejercicio.js
@@ -0,0 +1,9 @@
+// Initialization for ES Users
+import {
+    Collapse,
+    Ripple,
+    initTWE,
+} from "tw-elements";
+
+initTWE({ Collapse, Ripple });
+const html = document.querySelector("html")
